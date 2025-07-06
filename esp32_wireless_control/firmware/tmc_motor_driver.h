50df123ae61b4f4ea525c25c87287e555e02e5db
Sylensky <admin@medieval-realm.net>
2025-07-06 22:15:59 +0200

    wip: format code with clangd 20

--- esp32_wireless_control/firmware/tmc_motor_driver.h
+++ esp32_wireless_control/firmware/tmc_motor_driver.h
@@ -5,18 +5,17 @@
 #include <Arduino.h>
 #include <TMCStepper.h>
 
-class TmcMotorDriver : public MotorDriver
-{
-  public:
-    TmcMotorDriver(HardwareSerial* serialPort, uint8_t addr, float rSense, uint8_t rxPin,
-                   uint8_t txPin);
-    ~TmcMotorDriver(){};
-    void setMicrosteps(uint16_t microsteps);
+class TmcMotorDriver : public MotorDriver {
+public:
+  TmcMotorDriver(HardwareSerial *serialPort, uint8_t addr, float rSense,
+                 uint8_t rxPin, uint8_t txPin);
+  ~TmcMotorDriver(){};
+  void setMicrosteps(uint16_t microsteps);
 
-  private:
-    HardwareSerial* serialPort;
-    uint8_t addr;
-    TMC2209Stepper tmc_driver;
+private:
+  HardwareSerial *serialPort;
+  uint8_t addr;
+  TMC2209Stepper tmc_driver;
 };
 
 #endif /* _TMC_MOTOR_DRIVER_H_ */
