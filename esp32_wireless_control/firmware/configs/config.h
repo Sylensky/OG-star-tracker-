31461a3ae6df5d053777ad6bb35b45d0ba03fd3e
Sylensky <admin@medieval-realm.net>
2026-04-04 23:02:14 +0200

    firmware: board-config: move pin definition to own class initializer
    Hardware revision may contain pin configuration changes. We need to
    configure the pins only when we know on what hardware we are running.
    Misconfiguration could end up in damaging the board itself.

--- esp32_wireless_control/firmware/configs/config.h
+++ esp32_wireless_control/firmware/configs/config.h
@@ -5,8 +5,6 @@
 #include "wifi_config.h"
 #endif
 
-#include "pins_config.h"
-
 #define STEPPER_0_9 0 // 0.9 degree stepper motor
 #define STEPPER_1_8 1 // 1.8 degree stepper motor
 
@@ -68,14 +66,6 @@
 #endif
 /**********************/
 
-#define TMC_R_SENSE 0.11f // Match to your driver
-#define AXIS_SERIAL_PORT Serial2
-#define AXIS_RX 16
-#define AXIS_TX 19
-
-#define AXIS1_ADDR 0
-#define AXIS2_ADDR 1
-
 /**********************/
 // Configure microstepping driver type
 #define USE_MSx_PINS_MICROSTEPPING 1
@@ -93,27 +83,11 @@
 #define ARCSEC_PER_STEP 4.0
 #endif
 
-// LEDs for intervalometer status and general purpose status led
-#define INTERV_PIN 25
-#define STATUS_LED 26 // (Red)
+// Memory locations
 #define LANG_EEPROM_ADDR 0
 #define PRESETS_EEPROM_START_LOCATION 1
 // Start after intervalometer presets (10 * 28 bytes)
 #define TRACKING_RATE_PRESETS_EEPROM_START_LOCATION 300
-
-// Stepper driver pins -- intended for TMC2209 for now
-// AXIS 1 - RA
-#define AXIS1_STEP 5
-#define AXIS1_DIR 15
-#define SPREAD_1 4
-// AXIS 2 - DEC
-#define AXIS2_STEP 19
-#define AXIS2_DIR 18
-#define SPREAD_2 21
-// common pins
-#define RA_MS1 23
-#define RA_MS2 22
-#define EN12_n 17
 
 // Board Version
 #define BOARD_VERSION_PIN 2
---
+++ esp32_wireless_control/firmware/functions/board_version/board_config.cpp
@@ -0,0 +1,78 @@
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include "board_config.h"
+#include "board_version.h"
+#include "uart.h"
+
+BoardConfigManager::BoardConfigManager() : _config(nullptr), _initialized(false)
+{
+}
+
+BoardConfigManager::~BoardConfigManager()
+{
+    if (_config != nullptr)
+    {
+        delete _config;
+        _config = nullptr;
+    }
+}
+
+BoardConfigManager& BoardConfigManager::getInstance()
+{
+    static BoardConfigManager instance;
+    return instance;
+}
+
+void BoardConfigManager::init(uint8_t versionPin)
+{
+    if (_initialized)
+        return;
+
+    BoardVersion::getInstance().init(versionPin);
+    HardwareVersion hwVersion = BoardVersion::getInstance().getVersion();
+
+    switch (hwVersion)
+    {
+        case HardwareVersion::V1_X:
+            _config = new BoardConfigV1();
+            _initialized = true;
+            break;
+
+        case HardwareVersion::V2_2:
+            _config = new BoardConfigV2_2();
+            _initialized = true;
+            break;
+
+        case HardwareVersion::Unknown:
+        case HardwareVersion::MaxVersions:
+        default:
+            // SAFETY: Do NOT use fallback configuration for unknown hardware!
+            // Using wrong pin configuration could damage the board.
+            _config = nullptr;
+            _initialized = false;
+            print_out(
+                "ERROR: Unknown hardware version detected - cannot initialize board configuration");
+            while (true)
+            {
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+            }
+            break;
+    }
+}
+
+const BoardConfig& BoardConfigManager::getConfig() const
+{
+    if (!_initialized || _config == nullptr)
+    {
+        // SAFETY: Should never happen, but if it does, halt instead of returning invalid config
+        // Cannot use print_out here as UART queue may not be initialized yet
+        while (true)
+        {
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+
+    return *_config;
+}
